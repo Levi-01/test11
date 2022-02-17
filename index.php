f9304ef54c44fd0f8bc1a06832b75ba6cc3bb02b
IBOY <69951026+Levi-01@users.noreply.github.com>
2022-02-17 16:10:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
-<?php
-$target = 'source.ext'; // This is the file that already exists
-$link = 'https://iframe.dacast.com/vod/1391e0a4-3248-f807-5169-87c66aaab840/369060c9-d1b7-9c64-5c2f-bf6258c6c4b2'; // This the filename that you want to link it to
-
-link($target, $link);
-?>
+https://iframe.dacast.com/vod/1391e0a4-3248-f807-5169-87c66aaab840/369060c9-d1b7-9c64-5c2f-bf6258c6c4b2
